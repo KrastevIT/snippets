1e63ec4b791dd2f904dc0ff3270e26b914694742
Kristiyan Krastev <49103119+KrastevIT@users.noreply.github.com>
2022-06-18 16:50:41 +0300

    Update outside-click.js

--- outside-click/outside-click.js
+++ outside-click/outside-click.js
@@ -1,7 +1,4 @@
 $(document).on('click', function (e) {
-  event.preventDefault();
-  event.stopPropagation();
-
   const target = $(e.target);
   const outside = target.closest('.selector').length === 0
 
