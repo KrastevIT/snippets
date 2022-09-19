7632e12c4549360c50b6618ce4e604c5393f873b
Kristiyan Krastev <49103119+KrastevIT@users.noreply.github.com>
2022-09-19 15:04:42 +0300

    Update image-zoom.js

--- custom-zoom/image-zoom.js
+++ custom-zoom/image-zoom.js
@@ -10,43 +10,44 @@
       this.result = this.querySelector('#myresult');
       this._offsetWidth = this.result.offsetWidth;
       this._offsetHeight  = this.result.offsetHeight;
-
-      this.lens = document.createElement("DIV");
-      this.lens.setAttribute("class", "img-zoom-lens");
-      this.img.parentElement.insertBefore(this.lens, this.img);
-
-      this.cx = this.result.offsetWidth / this.lens.offsetWidth;
-      this.cy = this.result.offsetHeight / this.lens.offsetHeight;
+      this.cx = this._offsetWidth / 2;
+      this.cy = this._offsetWidth / 2;
+      this.magnifier = 2;
     }
 
     connectedCallback() {
       this.initZoomImage();
-     this.img.addEventListener("mousemove", this.move.bind(this));
+      this.img.addEventListener("mousemove", this.move.bind(this));
+      this.img.addEventListener("mouseout", this.stopMove.bind(this));
     }
 
     initZoomImage() {
       this.result.style.backgroundImage = "url('" + this.img.src + "')";
-      this.result.style.backgroundSize = (this.img.width * this.cx) + "px " + (this.img.height * this.cy) + "px";
+      this.result.style.backgroundSize = (this.img.width * this.magnifier) + "px " + (this.img.height * this.magnifier) + "px";
     }
 
     move(e) {
       e.preventDefault();
+
+      this.result.classList.remove('hidden');
+
       const pos = this.getCursorPos(e);
 
       this.result.style.left = pos.x + "px";
       this.result.style.top = pos.y + "px";
+      this.result.style.backgroundPosition = "-" + (pos.x * this.magnifier - this.cx) + "px -" + (pos.y * this.magnifier - this.cy) + "px";
+    }
 
-      this.result.style.backgroundPosition = "-" + (pos.x * this.cx) + "px -" + (pos.y * this.cy) + "px";
+    stopMove(e) {
+      e.preventDefault();
 
-      this.result.style.left = pos.x + "px";
-      this.result.style.top = pos.y + "px";
-
-      this.lens.style.left = pos.x + "px";
-      this.lens.style.top = pos.y + "px";
+      this.result.classList.add('hidden');
     }
 
     getCursorPos(e) {
-      var a, x = 0, y = 0;
+      let a = 0;
+      let x = 0;
+      let y = 0;
       e = e || window.event;
       /*get the x and y positions of the image:*/
       a = this.img.getBoundingClientRect();
