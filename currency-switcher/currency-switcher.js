39e09eebe5640de27c71d2d9475527ff68c1d9d1
Kristiyan Krastev <49103119+KrastevIT@users.noreply.github.com>
2022-10-18 17:14:22 +0300

    Update currency-switcher.js

--- currency-switcher/currency-switcher.js
+++ currency-switcher/currency-switcher.js
@@ -1,3 +1,44 @@
-$('.js-currency input').change(function (e) {
-  e.target.form.submit();
-});
+(() => {
+  if (customElements.get('currency-switcher')) {
+    return;
+  }
+
+  class CurrencySwitcher extends HTMLElement {
+    constructor() {
+      super();
+      this.inputs = this.querySelectorAll('input');
+      this.toggleElement = this.querySelector('.js-toggle');
+    }
+
+    connectedCallback() {
+      this.inputs.forEach(input => {
+        input.addEventListener('change', this.submit.bind(this));
+      })
+
+      this.toggleElement.addEventListener('click', this.toggle.bind(this));
+      document.addEventListener('click', this.close.bind(this));
+    }
+
+    submit(event) {
+      event.target.form.submit();
+    }
+
+    toggle(event) {
+      event.preventDefault();
+
+      if(this.hasAttribute('open')) {
+        this.removeAttribute('open');
+      } else {
+        this.setAttribute('open', '');
+      }
+    }
+
+    close(event) {
+     if(!event.target.closest('currency-switcher')) {
+      this.removeAttribute('open');
+     }
+    }
+  }
+
+  customElements.define('currency-switcher', CurrencySwitcher);
+})();
